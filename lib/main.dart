17315a91fe5d91d98641b3715107c5329dc68137
FahmiChbil <fchbil143@gmail.com>
2023-09-29 12:32:14 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:mylasttravelapp/Auth/presentation/pages/splash_page.dart';
-import 'package:mylasttravelapp/Hotel/domain/entities/oneHotel.dart';
 import 'package:mylasttravelapp/Hotel/presentation/Cubit/cubit/all_hotels_cubit.dart';
+import 'package:mylasttravelapp/Hotel/presentation/Cubit/cubit/pay_cubit_cubit.dart';
 import 'package:mylasttravelapp/Hotel/presentation/Hotels_cubit/hotels_cubit.dart';
 import 'package:mylasttravelapp/Hotel/presentation/pages/first_page.dart';
-import 'package:mylasttravelapp/Hotel/presentation/pages/hotel_detail_page.dart';
+import 'package:mylasttravelapp/admin/presentation/cubit/edit_data_cubit.dart';
+import 'package:mylasttravelapp/admin/presentation/pages/add_data_page.dart';
+import 'package:mylasttravelapp/admin/presentation/pages/chat_page.dart';
+import 'package:mylasttravelapp/core/const/stripe_keys.dart';
 
 import 'Auth/presentation/bloc/cubit/auth_cubit.dart';
 import 'Auth/presentation/pages/sign_up_page.dart';
@@ -15,6 +19,8 @@ import 'core/const/themes.dart';
 import 'ijnection.dart' as di;
 
 void main() async {
+  Stripe.publishableKey = ApiKeys.publishablekey;
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await di.init();
@@ -38,11 +44,19 @@ class MyApp extends StatelessWidget {
           ),
           BlocProvider(
             create: (context) => di.sl<AllHotelsCubit>(),
+          ),
+          BlocProvider(
+            create: (context) => di.sl<PayCubitCubit>(),
+          ),
+          BlocProvider(
+            create: (context) => di.sl<EditDataCubit>(),
           )
         ],
         child: MaterialApp(
           theme: appTheme,
           routes: {
+            ChatPageAdmin.id: (context) => ChatPageAdmin(),
+            AddDataPage.id: (context) => AddDataPage(),
             FirstPage.id: (context) => FirstPage(),
             SignUp.id: (context) => const SignUp(),
           },
