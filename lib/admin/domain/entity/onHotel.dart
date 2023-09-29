5208d63ae4eddd51c5094192f400cdda46a84748
FahmiChbil <fchbil143@gmail.com>
2023-09-29 12:33:55 +0200

    entitie

---
+++ lib/admin/domain/entity/onHotel.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: must_be_immutable, file_names
+
+import 'package:equatable/equatable.dart';
+
+class OneHotel1 extends Equatable {
+  List<dynamic>? activities;
+  String? etoiles;
+  List<dynamic>? images;
+  String? info;
+  String? name;
+  String? placeName;
+  String? note;
+  String? prix;
+  @override
+  List<Object?> get props => [];
+}
